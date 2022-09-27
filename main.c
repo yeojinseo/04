4f1839e56fdebd94bdfb274fbb470ea032852623
yeojinseo <113421483+yeojinseo@users.noreply.github.com>
2022-09-27 17:20:24 +0900

    06

--- main.c
+++ main.c
@@ -6,13 +6,10 @@
 int main(int argc, char *argv[]) {
 	int x;
 	
-	printf("input the year : ");
+	printf("input the second : ");
 	scanf("%i", &x);
 	
-	if ((x%4 == 0 && x%100 != 0) || x%400 == 0)
-		printf("%i is leap year!\n", x);
-	else
-		printf("%i is not leap year!\n", x);
-
+	printf("The time for %i second is %i:%i:%i\n", x, x/3600, (x%3600)/60, (x%3600)%60);
+	
 	return 0;
 }
